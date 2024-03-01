e5fcfba59b40809912f5615753a778bb51ca3cae
Felix Doktorman <doctorf72@users.noreply.github.com>
2024-03-01 20:48:30 +0200

    Small enhancements

--- WorldClock.py
+++ WorldClock.py
@@ -5,7 +5,7 @@ from tkinter import *
 import time
 
 root = Tk()
-root.geometry("315x175")
+root.geometry("325x175")
 root.config(bg="black")
 
 
@@ -14,25 +14,25 @@ def times():
    
     home = pytz.timezone('Asia/Jerusalem')
     local_time = datetime.now(home)
-    current_time = local_time.strftime("WW%U %x\n%a  %I:%M%p")
+    current_time = local_time.strftime("WW%U %x\n%a  %I:%M:%S%p")
     clock.config(text=current_time, bg="black", fg= "white")
     name.config(text = 'Israel', bg="black", fg= "white")
 
     home = pytz.timezone('Europe/Dublin')
     local_time = datetime.now(home)
-    current_time = local_time.strftime("WW%U %x\n%a  %I:%M%p")
+    current_time = local_time.strftime("WW%U %x\n%a  %I:%M:%S%p")
     clock1.config(text=current_time, bg="black", fg= "white")
     name1.config(text='Ireland', bg="black", fg= "white")
 
     home = pytz.timezone('America/Phoenix')
     local_time = datetime.now(home)
-    current_time = local_time.strftime("WW%U %x\n%a  %I:%M%p")
+    current_time = local_time.strftime("WW%U %x\n%a  %I:%M:%S%p")
     clock2.config(text=current_time, bg="black", fg= "white")
     name2.config(text='Phoenix[AZ]', bg="black", fg= "white")
 
     home = pytz.timezone('US/Pacific')
     local_time = datetime.now(home)
-    current_time = local_time.strftime("WW%U %x\n%a  %I:%M%p")
+    current_time = local_time.strftime("WW%U %x\n%a  %I:%M:%S%p")
     clock3.config(text=current_time, bg="black", fg= "white")
     name3.config(text='Portland[OR]', bg="black", fg= "white")
 
@@ -42,25 +42,25 @@ def times():
 LabelSize = 15
 ClockSize = 15
 
-name = Label(root, font=("times",LabelSize,"bold"))
-name.place(x=35, y=5)
+name = Label(root, font=("times",LabelSize,"bold italic underline"))
+name.place(x=40, y=5)
 clock=Label(root, font=("times",ClockSize,"bold"))
 clock.place(x=10, y=30)
 
-name1 = Label(root, font=("times",LabelSize,"bold"))
-name1.place(x=185, y=5)
+name1 = Label(root, font=("times",LabelSize,"bold italic underline"))
+name1.place(x=190, y=5)
 clock1=Label(root, font=("times",ClockSize,"bold"))
-clock1.place(x=160, y=30)
+clock1.place(x=165, y=30)
 
-name2 = Label(root, font=("times",LabelSize,"bold"))
+name2 = Label(root, font=("times",LabelSize,"bold italic underline"))
 name2.place(x=20, y=85)
 clock2=Label(root, font=("times",ClockSize,"bold"))
 clock2.place(x=10, y=110)
 
-name3 = Label(root, font=("times",LabelSize,"bold"))
-name3.place(x=185, y=85)
+name3 = Label(root, font=("times",LabelSize,"bold italic underline"))
+name3.place(x=180, y=85)
 clock3=Label(root, font=("times",ClockSize,"bold"))
-clock3.place(x=160, y=110)
+clock3.place(x=165, y=110)
 
 
 times()
